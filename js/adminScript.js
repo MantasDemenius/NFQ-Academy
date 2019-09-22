7003d7a1a02c8679bfe595ab798054b7894d8364
Tidish <tidish@inbox.lt>
2019-09-22 21:30:47 +0300

    Added svieslentes display

--- js/adminScript.js
+++ js/adminScript.js
@@ -3,35 +3,51 @@ const inputSpecialist = document.getElementById('addSpecialist');
 
 function loadJson() {
   var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                console.log(data);
-                arraySort(data["clients"]);
-                localStorage.setItem('clientData', JSON.stringify(data));
-            }
-        }
-    };
-    httpRequest.open('GET', './json/InitialData.json');
-    httpRequest.send();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        console.log(data);
+        arraySort(data["clients"]);
+        localStorage.setItem('clientData', JSON.stringify(data));
+      }
+    }
+  };
+  httpRequest.open('GET', './json/InitialData.json');
+  httpRequest.send();
 }
 
 function addForm() {
-  const itemsArray =
-    {
+  const parsed = JSON.parse(localStorage.getItem('clientData')) ? JSON.parse(localStorage.getItem('clientData')) : false;
+  if (parsed === false) {
+    const itemsArray = {
+      "specialists": [{
+        "name": inputSpecialist.value
+      }],
+      "clients": [{
         "name": inputName.value,
         "number": 10,
         "state": true,
         "specialist": inputSpecialist.value
-      }
-  const parsed = JSON.parse(localStorage.getItem('clientData'));
-  let clientLength = parsed["clients"].length;
-  parsed["clients"][clientLength] =  itemsArray;
-  arraySort(parsed["clients"]);
-  localStorage.setItem('clientData', JSON.stringify(parsed));
-}
+      }]};
+      localStorage.setItem('clientData', JSON.stringify(itemsArray));
 
-const arraySort = (data) => {
-  data.sort((a, b) => (a.specialist > b.specialist) ? 1 : (a.specialist === b.specialist) ? ((a.number > b.number) ? 1 : -1) : -1 );
-}
+    }
+    else {
+      const itemsArray = {
+        "name": inputName.value,
+        "number": 10,
+        "state": true,
+        "specialist": inputSpecialist.value
+      };
+      let clientLength = parsed["clients"].length;
+      console.log(clientLength);
+      parsed["clients"][clientLength] = itemsArray;
+      arraySort(parsed["clients"]);
+      localStorage.setItem('clientData', JSON.stringify(parsed));
+    }
+  }
+
+  const arraySort = (data) => {
+    data.sort((a, b) => (a.specialist > b.specialist) ? 1 : (a.specialist === b.specialist) ? ((a.number > b.number) ? 1 : -1) : -1);
+  }
---
+++ js/telegraph.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  const items = JSON.parse(localStorage.getItem('clientData'));
+  // items["specialists"].map(person => {
+  //   const th = document.createElement('th');
+  //   th.className = "cell100 column";
+  //   th.textContent = person.name;
+  //   document.getElementById('table-head').appendChild(th);
+  // });
+  items["clients"].map(person => {
+    if(person.state) {
+      const tr = document.createElement('tr');
+      const td1 = document.createElement('td');
+      const td2 = document.createElement('td');
+      td1.className = "cell100 column";
+      td2.className = "cell100 column";
+      td1.textContent = person.specialist;
+      td2.textContent = person.number;
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      document.getElementById('table-body').appendChild(tr);
+      // document.getElementById('table-body').appendChild(td2);
+    }
+  });
+}

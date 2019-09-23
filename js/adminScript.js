338ce909d061bdf7ac97d5fe3f5c546cfabce157
Tidish <tidish@inbox.lt>
2019-09-23 20:20:15 +0300

    Level 1 done

--- js/adminScript.js
+++ js/adminScript.js
@@ -7,7 +7,7 @@ function loadJson() {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
         var data = JSON.parse(httpRequest.responseText);
-        console.log(data);
+        // console.log(data);
         arraySort(data["clients"]);
         localStorage.setItem('clientData', JSON.stringify(data));
       }
@@ -19,7 +19,8 @@ function loadJson() {
 
 function addForm() {
   const parsed = JSON.parse(localStorage.getItem('clientData')) ? JSON.parse(localStorage.getItem('clientData')) : false;
-
+  let optionSpecialistText = inputSpecialist.options[inputSpecialist.selectedIndex].text;
+  let newNumber = getNumber(optionSpecialistText, parsed["clients"]);
   if (parsed === false) {
     console.log('Neuzkrauti dumoenys');
 
@@ -27,12 +28,11 @@ function addForm() {
     else {
       const itemsArray = {
         "name": inputName.value,
-        "number": 10,
+        "number": newNumber,
         "state": 'Neaptarnautas',
-        "specialist": inputSpecialist.options[inputSpecialist.selectedIndex].text
+        "specialist": optionSpecialistText
       };
       let clientLength = parsed["clients"].length;
-      console.log(clientLength);
       parsed["clients"][clientLength] = itemsArray;
       arraySort(parsed["clients"]);
       localStorage.setItem('clientData', JSON.stringify(parsed));
@@ -42,3 +42,14 @@ function addForm() {
   const arraySort = (data) => {
     data.sort((a, b) => (a.specialist > b.specialist) ? 1 : (a.specialist === b.specialist) ? ((a.number > b.number) ? 1 : -1) : -1);
   }
+
+  function getNumber(specialist, items) {
+  let number = 0;
+  items.map(person => {
+    if(person.specialist === specialist && number <= person.number){
+       number = person.number +1;
+    }
+  })
+
+  return number;
+}
